c9c630894516bd2c3e2589c7028e4f2c126c520b
karl-henrik <kh@mean.se>
2018-11-13 11:34:00 +0100

    Added the API project to hold the text service

---
+++ src/TextService.API/Controllers/TextController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TextService.API.Models;
+
+namespace TextService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TextController : ControllerBase
+    {
+        // POST api/text/charactercount/
+        [HttpPost("characterCount/")]
+        public ActionResult<int> CharacterCount([FromBody] InputModel input)
+        {
+            return input.Text.Length;
+        }
+
+        // POST api/text/wordcount/
+        [HttpPost("wordCount/")]
+        public ActionResult<int> WordCount ([FromBody] InputModel input)
+        {
+            return input.Text.Split(" ").Length;
+        }
+
+    }
+}
